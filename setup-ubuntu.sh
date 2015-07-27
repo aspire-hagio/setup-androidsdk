022146ed8889810a010cd66371ccc18d9212a478
Daiki Hagio <hagio@aspire-inc.jp>
2015-07-27 12:42:53 +0900

    add file

---
+++ setup-ubuntu.sh
@@ -0,0 +1,10 @@
+add-apt-repositry ppa:webupd8team/java
+apt-get update
+apt-get install -y oracle-java7-installer
+
+apt-get install -y vnc4server
+
+apt-get install -y gnome-core
+
+apt-get install -y git
+
